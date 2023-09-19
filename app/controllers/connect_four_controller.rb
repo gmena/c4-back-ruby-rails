7550cb78486fc525b9b21ef8eed3fd8c1c2aab84
Gonzalo Mena-Mendoza <gonzalo@mena.com.mx>
2023-09-18 21:50:28 -0600

    Add preliminary controller

---
+++ app/controllers/connect_four_controller.rb
@@ -0,0 +1,5 @@
+class ConnectFourController < ApplicationController
+  def index
+    render json: {}
+  end
+end
---
+++ test/controllers/connect_four_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class ConnectFourControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
